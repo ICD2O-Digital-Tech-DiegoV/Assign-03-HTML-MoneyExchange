438bd4a8d6cb3b4e7250c7b0b3938bd7c68a5401
DiegoValdivieso22 <159164145+DiegoValdivieso22@users.noreply.github.com>
2024-04-12 15:43:58 +0000

    EDit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function doMathClicked() {
+  // this function does basic math: Mph to Kph
+  let USD = document.getElementById("USD").value;
+  let answer = USD * 1.37;
+
+  // Make sure not negative
+  if (answer < 0) {
+    answer = 0;
+    document.getElementById("Kp").innerHTML = "Invalid input. Ensure value is a positive number.";
+  }
+  else {
+    document.getElementById("CAD").innerHTML = "CAD = " + answer.toFixed(2);
+  }
+}
+function kphToMetersPH() {
+  let USD = document.getElementById("USD").value;
+  let calculation = USD * 92.90;
+
+  // Make sure not negative
+  if (calculation <= 0) {
+    calculation = 0;
+    document.getElementById("Rouble").innerHTML = "Invalid input. Ensure value is a positive number.";
+  }
+  else {
+    document.getElementById("Rouble").innerHTML = "Russian Roubles = " + calculation.toFixed(2);
+
+  }
+}
